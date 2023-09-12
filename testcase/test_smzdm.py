cfbaef19f2e72e2e47023d16073ef328fcbe403d
Eric <hccgk@sina.com>
2023-09-12 23:01:30 +0800

    ui case

--- testcase/test_smzdm.py
+++ testcase/test_smzdm.py
@@ -5,11 +5,11 @@ from selenium.webdriver import Keys
 from selenium.webdriver.common.by import By
 
 if __name__ == '__main__':
-    pytest.main(['-sv', '--html=m_report.html'])
+    pytest.main(['-sv', 'test_smzdm.py','--html=m_report.html'])
 @pytest.fixture
 def driver():
     driver = webdriver.Chrome()
-    driver.implicitly_wait(20)
+    driver.implicitly_wait(5)
     driver.get("https://www.smzdm.com/")
     # driver.maximize_window()
     yield driver
@@ -18,11 +18,8 @@ def driver():
 
 
 class TestMZDM:
-    # driver = None
-    # def __init__(self):
-    #     self.driver = None
-    #
-    @pytest.mark.usefixtures("driver")
+
+    # @pytest.fixture
     def test_01search_phone(self, driver):
         input_key = driver.find_element(By.XPATH, '//*[@id="J_search_input"]')
         input_key.send_keys("手机", Keys.ENTER)
@@ -30,7 +27,6 @@ class TestMZDM:
         print(driver.find_element(By.XPATH, '//*[@id="feed-main-list"]/li[1]/div/div[2]/div[2]').text)
         assert driver.title == "手机_综合_什么值得买"
 
-    @pytest.mark.usefixtures("driver")
     def test_02search_tv(self, driver):
         input_key = driver.find_element(By.XPATH, '//*[@id="J_search_input"]')
         input_key.send_keys("电视", Keys.ENTER)
@@ -38,7 +34,6 @@ class TestMZDM:
         print(driver.find_element(By.XPATH, '//*[@id="feed-main-list"]/li[1]/div/div[2]/div[2]').text)
         assert driver.title == "电视_综合_什么值得买"
 
-    @pytest.mark.usefixtures("driver")
     def test_03search_bike(self, driver):
         input_key = driver.find_element(By.XPATH, '//*[@id="J_search_input"]')
         input_key.send_keys("电动自行车", Keys.ENTER)
